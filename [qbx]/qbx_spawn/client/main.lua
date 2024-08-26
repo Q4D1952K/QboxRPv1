158d323fe5ef47bbcae00d4174eaa61265f7c5fa
Q4D1952K <97731242+Q4D1952K@users.noreply.github.com>
2024-08-26 17:34:21 +0700

    fix spawn house

--- [qbx]/qbx_spawn/client/main.lua
+++ [qbx]/qbx_spawn/client/main.lua
@@ -254,7 +254,7 @@ RegisterNetEvent('qb-spawn:client:setupSpawns', function(cData, new, apps)
         local houses = lib.callback.await('qbx_spawn:server:getHouses')
         for i = 1, #houses do
             spawns[#spawns + 1] = houses[i]
-            spawns[#spawns].coords = json.decode(spawns[#spawns].door)
+            spawns[#spawns].coords = json.decode(spawns[#spawns].door_data)
             print(spawns[#spawns].coords)
         end
     else

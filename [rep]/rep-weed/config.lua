8bb76cff42626e909baf9e8b2e6174ba8efacaa9
BahnMiFPS <97731242+Q4D1952K@users.noreply.github.com>
2024-09-03 13:40:10 -0400

    z

--- [rep]/rep-weed/config.lua
+++ [rep]/rep-weed/config.lua
@@ -4,7 +4,7 @@ Config.UseTalkNPC = false    -- means use rep-talkNPCv2
 Config.QBDefault = false    -- If u want use qb-menu, qb-input
 Config.Inventory = 'ox'     --qs, old-qb, new-qb
 Config.Debug = true         -- debug soils. If true, plant down a plant, and see the soil hashes in the f8 console
-Config.HyperData = false -- If don't want hyperdata transfer, turn off it
+Config.HyperData = true -- If don't want hyperdata transfer, turn off it
 
 Config.Items = {
     ['femaleseed'] = 'femaleseed',

5724eba0bff017c47fe0d9ca21bd61a4128d10c9
Q4D1952K <97731242+Q4D1952K@users.noreply.github.com>
2024-09-27 15:34:09 +0700

    Update config.lua

--- [rep]/rep-boxGame/config.lua
+++ [rep]/rep-boxGame/config.lua
@@ -1,9 +1,11 @@
 Config = {}
 Config.Lang = {
     en = {
+       ui = {
         subtitle = "Product",
         instruction = "Pack into the box, seal the bottom, and close securely with tape",
         mouseInstruction = "Drag the mouse to seal and close the box",
+       }
     },
 }
 
